cb5f4ac3d30ca5c346f6e7aadffb30473844e73d
Wei Luo <luoweihoo@yahoo.com>
2019-08-24 08:47:12 +0800

    Initial commit

---
+++ Chapter02/Chapter02_Codes.js
@@ -0,0 +1,23 @@
+// let theNumber = Number(prompt('Pick a number:'));
+// if (!Number.isNaN(theNumber)) {
+//     console.log('Your number is the square root of ' + 
+//     theNumber * theNumber);
+// } else {
+//     console.log("Why didn't you give me a number?")
+// }
+
+// let theNumber = 0;
+// while (theNumber <= 12) {
+//     console.log("The number is " + theNumber);
+//     theNumber += 2;
+// }
+
+// for (let number = 0; number <= 20; number += 2) {
+//     console.log(number);
+// }
+
+let result = 1;
+for (let counter = 0; counter < 10; counter += 1) {
+    result = result * 2
+}
+console.log(result)
